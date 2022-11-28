c2cfde5358aff6bae58fa93325bf3a171ea6ad86
kietedwardo <ryuzin790@gmail.com>
2022-11-28 14:01:27 +0700

    test

--- app/src/main/java/com/example/model/Users.java
+++ app/src/main/java/com/example/model/Users.java
@@ -8,8 +8,8 @@ public class Users {
     private Integer Phone;
     private String Email;
     private Integer Birth;
-    private byte[] Photo;   /////
-
+    private byte[] Photo;  
+////
     public Users(Integer ID, String username, String password, String fullname, Integer phone, String email, Integer birth, byte[] photo) {
         this.ID = ID;
         this.Username = username;
