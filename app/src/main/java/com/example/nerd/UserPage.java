a7843a6f8523841115432c0d105004551d144c04
NgocAnhTu <anhttn20411@st.uel.edu.vn>
2022-12-02 13:05:09 +0700

    edit nav

--- app/src/main/java/com/example/nerd/UserPage.java
+++ app/src/main/java/com/example/nerd/UserPage.java
@@ -1,8 +1,15 @@
 package com.example.nerd;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Calendar;
 
 public class UserPage extends AppCompatActivity {
 
@@ -10,5 +17,37 @@ public class UserPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_page);
+        bottomNav();
+    }
+    private void bottomNav() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.user);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.Homepage:
+                        Intent courses = new Intent(UserPage.this, homepage.class);
+                        startActivity(courses);
+                        return true;
+
+                    case R.id.user:
+                        return true;
+
+                    case R.id.calendar:
+                        Intent user = new Intent(UserPage.this, Calendar.class);
+                        startActivity(user);
+                        return true;
+                    case R.id.courses:
+                        Intent calendar = new Intent(UserPage.this, lichhoc.class);
+                        startActivity(calendar);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
     }
 }
