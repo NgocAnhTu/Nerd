50584393e4304e693d8135518e6bd4accfe00865
mudfishnemn <linhltg20411@st.uel.edu.vn>
2022-12-05 20:29:29 +0700

    update phan thong bao

--- app/src/main/java/com/example/nerd/Notification.java
+++ app/src/main/java/com/example/nerd/Notification.java
@@ -39,12 +39,11 @@ public class Notification extends AppCompatActivity {
 
     private void initData() {
         notifications = new ArrayList<>();
-        notifications.add(new com.example.model.Notifications("Bạn còn 1 tiếng trước buổi học Ielts của giáo viên Trần Dần"));
+        notifications.add(new com.example.model.Notifications("Bạn còn 1 tiếng trước buổi học Ielts của giáo viên Andrew"));
         notifications.add(new com.example.model.Notifications("Còn 1 ngày nữa là hạn chót nộp bài viết Writing"));
-        notifications.add(new com.example.model.Notifications("Tài khoản Premium của bạn đã được kích hoạt thành công"));
         notifications.add(new com.example.model.Notifications("Cảm ơn bạn đã đánh giá giảng viên. Những feedback của bạn là nguồn động lực để giúp Nerd phát triển hơn"));
-        notifications.add(new com.example.model.Notifications("Giáo viên Tường Thanh đã update tài liệu sau buổi học ngày 28/11/2021. Chúc bạn học tập vui vẻ."));
-        notifications.add(new com.example.model.Notifications("Giáo viên Trần Tùng đã update tài liệu sau buổi học ngày 1/11/2021. Chúc bạn học tập vui vẻ."));
+        notifications.add(new com.example.model.Notifications("Giáo viên Tường Thanh đã update tài liệu sau buổi học ngày 20/11/2022. Chúc bạn học tập vui vẻ."));
+        notifications.add(new com.example.model.Notifications("Giáo viên Trần Tùng đã update tài liệu sau buổi học ngày 22/11/2021. Chúc bạn học tập vui vẻ."));
         notificationAdapter = new ItemNotificationAdapter(this, notifications);
         lvNotification.setAdapter(notificationAdapter);
     }
